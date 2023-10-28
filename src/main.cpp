a9501a51c47b1ee33fe211b17c9b36d22677aca8
Yuriy Sydor <yuriysydor1991@gmail.com>
2023-10-28 21:24:50 +0300

    CMakeLists.txt and simple demo source file add

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by cc on 28.10.23.
+//
+
+#include <iostream>
+
+int main (int argc, char** argv)
+{
+  std::cout << "Hello, World! It's simple CMake intro project!" << std::endl ;
+}
